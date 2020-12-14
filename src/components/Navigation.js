89e9e32c9b6801288c6ca9859f4c4867080371bb
IanMFraser <hunter_gracchus@hotmail.com>
2020-12-14 12:57:12 -0500

    ul semantics

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,15 +4,19 @@ const Navigation = () => {
     return(
         <nav className="navigation">
             <ul>
-                <ul className="nav-sections">
-                    <li><a href="http://localhost:3000/">Features</a></li>
-                    <li><a href="http://localhost:3000/">Pricing</a></li>
-                    <li><a href="http://localhost:3000/">Resources</a></li>
-                </ul>
-                <ul className="nav-sections right">
-                    <li><a href="http://localhost:3000/">Login</a></li>
-                    <li><button className="button sign-up-button">Sign Up</button></li>
-                </ul>
+                <li>
+                    <ul className="nav-sections">
+                        <li><a href="http://localhost:3000/">Features</a></li>
+                        <li><a href="http://localhost:3000/">Pricing</a></li>
+                        <li><a href="http://localhost:3000/">Resources</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <ul className="nav-sections right">
+                        <li><a href="http://localhost:3000/">Login</a></li>
+                        <li><button className="button sign-up-button">Sign Up</button></li>
+                    </ul>
+                </li>
             </ul>
         </nav>
     )
