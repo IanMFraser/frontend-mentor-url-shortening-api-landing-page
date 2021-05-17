5b5f51410e0a2b98e5cdfc710d0c079f35735fd2
IanMFraser <hunter_gracchus@hotmail.com>
2021-05-17 13:12:12 -0400

    connect front and back ends

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 //add express port
@@ -14,6 +15,17 @@ connectDB();
 //return response for testing 
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+
+//allow cors
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "www.ifurl.page");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
